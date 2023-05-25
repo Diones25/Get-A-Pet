7e832c88bec0657cf3fe011e2be073b54333de64
Diones <pereiradiones987@gmail.com>
2023-05-25 17:35:33 -0300

    mudando o link do mongodb

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 const mongoose = require('mongoose');
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/getapet');
+    await mongoose.connect('mongodb://127.0.0.1:27017/getapet');
     console.log(chalk.bgGreenBright.black('Conectou ao Mongose!'))
 }
 
